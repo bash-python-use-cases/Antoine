221687e8a2253c6ad4c016039d3cb5e506052034
root <root@localhost.localdomain>
2024-08-01 13:24:07 +0200

    script1.py

---
+++ script1.py
@@ -0,0 +1,9 @@
+def main():
+    print("Coucou petite perruche!")
+
+if __name__ == "__main__":
+    main()
+
+    print('Quel est ton nom ?')
+    mon_nom = input()
+    print('Salut, {}'.format(mon_nom))
