2252555aed2b8a7edbaf4bb33b8947788c15ad54
MannekenFrite <109413296+MannekenFrite@users.noreply.github.com>
2024-08-01 14:06:11 +0200

    correction script1.sh

--- script1.sh
+++ script1.sh
@@ -2,3 +2,15 @@
 python -c "def main():
     print('Coucou')
 main()"
+
+# Correction
+############
+##!/bin/bash
+
+#helloworld () {
+#python -c "def main():
+#    print('bonjour')
+#main()" || exit 1
+#}
+#
+#helloworld
